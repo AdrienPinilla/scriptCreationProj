cf45675e28a699f4242b310cc658b660ecdadcf1
AdrienPinilla <adrienpinilla@gmail.com>
2022-02-24 16:55:40 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# scriptCreationProj
+# Scripte de Création de Projet
+
+Il permet de créer les dossiers et fichiers utile à un projet de développement web
+
+## Arborescence des fichiers lorsque toutes les options sont choisis
+```
+.
+├── createNewProjv2.bash
+├── css
+│   ├── reset.css
+│   └── style.css
+├── html
+│   └── index.html
+├── js
+│   └── main.js
+├── php
+│   └── main.php
+├── readme.md
+└── ressources
+```
+
+
+## Configuration du script
+
+Il vous demandera plusieurs options pour la création du projet
+
+1. Si vous voulez un dossier PHP
+   - Si vous choisissez yes : il créera un dossier PHP avec un fichier ___main.php___
+2. Si vous voulez un dossier JS
+   - Si vous choisisser yes : il créera un dossier JS avec un fichier ___main.js___
+3. Si vous voulez un reset CSS
+   - Si vous choisissez yes : il créera un fichier ___reset.css___ plus  un fichier ___style.css___ avec les links intégré dans votre fichier ___index.html___
+   - Si vous choisissez no : il créera seulement un fichier ___style.css___ avec le link intégré dans votre fichier ___index.html___
---
+++ createNewProjv2.bash
@@ -0,0 +1,199 @@
+# 
+# 
+#
+# 
+# Script de test pour créer un répertoire pour dev
+# 
+#
+# 
+#
+# 
+echo "###############################################################"
+echo "#                                                             #"
+echo "#   Bienvenue dans le script de création d'un nouveau projet  #"
+echo "#                 html / CSS / JS / PHP                       #"
+echo "#                                                             #"
+echo "###############################################################"
+echo
+read -p "         Voulez-vous créer un nouveau projet ? <y/n> : " confirm
+
+
+
+
+if [ $confirm == 'y' ]
+then
+
+    echo
+    echo "###############################################################"
+    echo 
+    read -p "         Voulez-vous créer un dossier PHP ? <y/n> : " phpChoix
+    echo
+    echo "###############################################################"
+    echo 
+    read -p "         Voulez-vous créer un dossier JS ? <y/n> : " jsChoix
+    echo
+    echo "###############################################################"
+    echo 
+    read -p "         Voulez-vous inserer un reset css ? <y/n> : " choixReset
+    echo
+
+
+
+#--------------------------Création ou non du dossier PHP-------------------------
+    if [ $phpChoix == 'y' ]
+    then
+        mkdir php
+        touch php/main.php
+    fi
+
+#--------------------------Création ou non du dossier JS-------------------------
+    if [ $jsChoix == 'y' ]
+    then
+        mkdir js
+        touch js/main.js
+    fi
+
+#--------------------------Création des dossiers de base-------------------------
+    mkdir css
+    mkdir html
+    mkdir ressources
+
+    touch css/style.css
+
+    touch readme.md
+    echo "# New Project
+    
+    Let's go to code !"> readme.md
+
+
+
+#--------------------------Création du reset ou non plus lien dans le html-------------------------
+    if [ $choixReset == 'y' ]
+    then
+
+    touch css/reset.css
+    echo "/* http://meyerweb.com/eric/tools/css/reset/ 
+v2.0 | 20110126
+License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+    display: block;
+}
+body {
+    line-height: 1;
+}
+ol, ul {
+    list-style: none;
+}
+blockquote, q {
+    quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content: '';
+    content: none;
+}
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}" > css/reset.css
+
+    
+
+
+#--------------------------Création du fichier index.html avec le reset-------------------------
+
+    touch html/index.html
+    echo "<!DOCTYPE html>
+<html lang=\"fr\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Document</title>
+    <link rel=\"stylesheet\" href=\"../css/resset.css\" />
+    <link rel=\"stylesheet\" href=\"../css/style.css\" />
+</head>
+<body>
+    
+</body>
+</html>" > html/index.html
+
+    else
+
+#--------------------------Création du fichier index.html sans le reset-------------------------
+
+    touch html/index.html
+    echo "<!DOCTYPE html>
+<html lang=\"fr\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Document</title>
+    <link rel=\"stylesheet\" href=\"../css/style.css\" />
+</head>
+<body>
+    
+</body>
+</html>" > html/index.html
+
+    fi
+
+    
+    echo "###############################################################"
+    echo "#                                                             #"
+    echo "#          Dossier crée avec les options suivante             #"
+    echo "#                                                             #"
+    echo "#          CSS reset <y/n> : $choixReset                                #"
+    echo "#          PHP       <y/n> : $phpChoix                                #"
+    echo "#          JS        <y/n> : $jsChoix                                #"
+    echo "#                                                             #"
+    echo "###############################################################"
+    echo
+    echo
+    read -p "Tapez entrer pour terminer" 
+    echo
+    echo "###############################################################"
+    echo "#                                                             #"
+    echo "#                   Ouverture de VS Code                      #"
+    echo "#                                                             #"
+    echo "###############################################################"
+
+    sleep 2
+    code .
+    exit 0
+
+else
+
+    echo "###############################################################"
+    echo "#                                                             #"
+    echo "#          La création du nouveau projet est annulé           #"
+    echo "#                                                             #"
+    echo "###############################################################"
+    exit 0
+fi
+
